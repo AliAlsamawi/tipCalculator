1255ecc6df01ca57033b3d0949c20c6920422613
Ali A <alsamawiali4@gmail.com>
2022-10-27 13:31:20 -0400

    writing the logic out

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 🌟 APP: Tip Calculator
 
 These are the 3 functions you must use 👇
-========================================
+=========================================
 calculateBill()
 increasePeople()
 decreasePeople()
@@ -19,3 +19,38 @@ These are all the DIV ID's you're gonna need access to 👇
 
 // Get global access to all inputs / divs here (you'll need them later 😘)
 // bill input, tip input, number of people div, and per person total div
+const billInput = document.getElementById("billTotalInput");
+const tipInput = document.getElementById("tipInput");
+const numberOfPeopleDiv = document.getElementById("numberOfPeople");
+const perPersonTotalDiv = document.getElementById("perPersonTotal");
+
+// Get number of people from number of people div
+
+// ** Calculate the total bill per person **
+
+const calculateBill = () => {
+  // get bill from user input & convert it into a number
+  // get the tip from user & convert it into a percentage (divide by 100)
+  // get the total tip amount
+  // calculate the total (tip amount + bill)
+  // calculate the per person total (total divided by number of people)
+  // update the perPersonTotal on DOM & show it to user
+};
+
+// ** Splits the bill between fewer people **
+
+const increasePeople = () => {
+  // increment the amount of people
+  // update the DOM with the new number of people
+  // calculate the bill based on the new number of people
+};
+
+// ** Splits the bill between fewer people **
+const decreasePeople = () => {
+  // guard clause
+  // if amount is 1 or less simply return
+  // (a.k.a you can't decrease the number of people to 0 or negative!)
+  // decrement the amount of people
+  // update the DOM with the new number of people
+  // calculate the bill based on the new number of people
+};
